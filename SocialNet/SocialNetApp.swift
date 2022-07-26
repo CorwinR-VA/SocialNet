22af10d794e04314ba086cf94407cc4ddf609f54
Corwin Rainier <corwinrainier89@gmail.com>
2022-07-26 13:28:08 -0300

    Initial Commit

---
+++ SocialNet/SocialNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetApp.swift
+//  SocialNet
+//
+//  Created by Corwin Rainier on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
